be106855df71177b2de169f6eb30ea4f6d924f9e
AlessandraMagri <screecedesign@hotmail.it>
2022-09-23 12:30:55 +0200

    modifica con mio nome

--- src/App.js
+++ src/App.js
@@ -2,26 +2,11 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  let ciao = 10;
   return (
-    <div>
-        <Cat/> {/* classe componente */}
-        {ciao}
-        <Dog/> {/* funzione componente */}
+    <div className='App'>
+        ciao sono Alessandra
     </div>
   );
 }
 
 export default App;
-
-class Cat extends React.Component {
-    render() {
-      return <p>Hi, I am a Cat!</p>;
-    }
-  }
-
-function Dog() {
-    return (
-      <p>Hi, I am a Dog!</p>
-    );
-  }
